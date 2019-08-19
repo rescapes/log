ec288b93c08f324f1d4acce1361ab80674024d80
Andy Likuski <andy@likuski.org>
2019-08-19 11:05:53 +0200

    Small fix to prevent multiple console logs

--- src/logHelpers.js
+++ src/logHelpers.js
@@ -67,7 +67,7 @@ loggers.add('rescapeDefault', rescapeDefault);
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' && !isBrowser()) {
   loggers.get('rescapeDefault').add(
     rescapeDefaultTransports.console
   );
